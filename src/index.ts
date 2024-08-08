30223cb1874c2651a96429d1be8ba9915ae91050
akashtiwariG <akashtiwariaku@gmail.com>
2024-08-08 10:52:45 +0530

    initiated

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
+import prisma from "./db";
+const app = express();
+app.use(cors());
+import adminRouter from "./routes/admin"
+import administratorRouter from "./routes/administrator"
+import issueRouter from "./db/issue"
+const axios = require('axios');
+// Middleware for parsing request bodies
+app.use(bodyParser.json());
+
+app.use("/admin", adminRouter)
+app.use("/administrator", administratorRouter)
+app.use("/issue",issueRouter)
+
+async function createAdministrator(id:number,email:string,password:string,phonenumber:number,status:string) {
+    const res = await prisma.administrators.create({
+        data:{
+            id,
+            email,
+            password,
+            phonenumber,
+            status
+        }
+    })
+    console.log(res);
+}
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
