59d9a99fe9da4ccda070cddb81d998d3dd84fcfd
akashtiwariG <akashtiwariaku@gmail.com>
2024-08-08 20:14:30 +0530

    a

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import cors from "cors"
 import bodyParser from "body-parser"
 import prisma from "./db";
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:["https://frontend1-elk2o97p2-tusharsaini-05s-projects.vercel.app/"],
+    methods:["POST","GET","DELETE"],
+    credentials:true
+}));
 import adminRouter from "./routes/admin"
 import administratorRouter from "./routes/administrator"
 import issueRouter from "./db/issue"
