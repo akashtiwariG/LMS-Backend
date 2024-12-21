a8f8ef4301f4fa5bd0b032df9f065e0cd5ebdc59
akashtiwariG <akashtiwariaku@gmail.com>
2024-12-21 11:26:11 +0530

    l

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ const axios = require('axios');
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
 
+
 app.use("/admin", adminRouter)
 app.use("/administrator", administratorRouter)
 app.use("/issue",issueRouter)
